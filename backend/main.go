09ba8b20114656fe06d5e077fe1447c75b9e4610
Vlad <vladyur4ik@gmail.com>
2024-04-16 22:35:17 +0200

    add cores rights

--- backend/main.go
+++ backend/main.go
@@ -12,12 +12,19 @@ var registeredUsers []structures.User
 func main() {
 	http.HandleFunc("/register", registerHandler)
 	http.HandleFunc("/login", loginHandler)
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		setupCORS(&w, r)
+		if r.Method == "OPTIONS" {
+			return
+		}
+	})
 
-	fmt.Println("Server is running on port 8080...")
+	fmt.Println("Server is running on port 8081...")
 	http.ListenAndServe(":8081", nil)
 }
 
 func registerHandler(w http.ResponseWriter, r *http.Request) {
+	setupCORS(&w, r)
 	var user structures.User
 	err := json.NewDecoder(r.Body).Decode(&user)
 	if err != nil {
@@ -30,6 +37,8 @@ func registerHandler(w http.ResponseWriter, r *http.Request) {
 }
 
 func loginHandler(w http.ResponseWriter, r *http.Request) {
+	setupCORS(&w, r)
+	w.Header().Set("Access-Control-Allow-Origin", "*")
 	var user structures.User
 	err := json.NewDecoder(r.Body).Decode(&user)
 	if err != nil {
@@ -45,4 +54,14 @@ func loginHandler(w http.ResponseWriter, r *http.Request) {
 	}
 
 	http.Error(w, "Unauthorized", http.StatusUnauthorized)
+}
+
+func setupCORS(w *http.ResponseWriter, req *http.Request) {
+	(*w).Header().Set("Access-Control-Allow-Origin", "*")
+	(*w).Header().Set("Access-Control-Allow-Methods", "POST, GET, OPTIONS, PUT, DELETE")
+	(*w).Header().Set("Access-Control-Allow-Headers", "Accept, Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization")
+	if req.Method == "OPTIONS" {
+		(*w).WriteHeader(http.StatusOK)
+		return
+	}
 }
