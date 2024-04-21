f528b35e9639a40dab881d89640851de3e34d8e5
AomineDai <132513241+AomineDai@users.noreply.github.com>
2024-04-21 20:51:54 +0300

    edit Validation

---
+++ frontend/login.js
@@ -0,0 +1,24 @@
+let btn = document.getElementById('.btn');
+function loginUser(email, password) {
+    fetch('http://localhost:8081/login', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+            email: email,
+            password: password
+        }),
+    })
+    .then(response => {
+        if (response.ok) {
+            console.log('Вход выполнен успешно!');
+        } else {
+            console.error('Ошибка при входе:', response.statusText);
+        }
+    })
+    .catch(error => {
+        console.error('Ошибка при отправке запроса:', error);
+    });
+}
+btn.addEventListener("click", loginUser)
---
+++ frontend/register.js
@@ -0,0 +1,25 @@
+let btn = document.getElementById('.btn');
+function registerUser(email, password) {
+    fetch('http://localhost:8081/register', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+            email: email,
+            password: password
+        }),
+    })
+    .then(response => {
+        if (response.ok) {
+            console.log('Пользователь зарегистрирован успешно!');
+        } else {
+            console.error('Ошибка при регистрации:', response.statusText);
+        }
+    })
+    .catch(error => {
+        console.error('Ошибка при отправке запроса:', error);
+    });
+}
+
+btn.addEventListener("click", registerUser)
