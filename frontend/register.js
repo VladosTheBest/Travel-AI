dd50423acf46ac316fa3084eca9ab80dafbd6032
AomineDai <132513241+AomineDai@users.noreply.github.com>
2024-04-23 20:38:33 +0300

    Reworking code.

--- frontend/register.js
+++ frontend/register.js
@@ -1,23 +1,55 @@
-let btn = document.querySelector('.btn');
-function registerUser(email, password) {
-    fetch('http://localhost:8081/register', {
+// let btn = document.querySelector('.btn');
+// function registerUser(email, password) {
+//     fetch('http://localhost:8081/register', {
+//         method: 'POST',
+//         headers: {
+//             'Content-Type': 'application/json',
+//         },
+//         body: JSON.stringify({
+//             email: email,
+//             password: password
+//         }),
+//     })
+    // .then(response => {
+    //     if (response.ok) {
+    //         console.log('Пользователь зарегистрирован успешно!');
+    //     } else {
+    //         console.error('Ошибка при регистрации:', response.statusText);
+    //     }
+    // })
+//     .catch(error => {
+//         console.error('Ошибка при отправке запроса:', error);
+//     });
+// }
+
+let form = document.getElementById('form');
+form.addEventListener('submit', function(e){
+    e.preventDefault()
+
+    let email = document.getElementById('email').value;
+    let password = document.getElementById('password').value;
+
+    fetch("http://localhost:8081/register",{
         method: 'POST',
-        headers: {
-            'Content-Type': 'application/json',
-        },
-        body: JSON.stringify({
-            email: email,
-            password: password
+        body:JSON.stringify({
+            email:email,
+            password:password
         }),
+        headers:{
+            "Content-Type":"application/json; charset=UTF-8"
+        }
     })
     .then(response => {
         if (response.ok) {
-            console.log('Пользователь зарегистрирован успешно!');
+            console.log('Login successful!');
         } else {
-            console.error('Ошибка при регистрации:', response.statusText);
+            console.error('Sign-in error:', response.statusText);
         }
     })
+    // .then(function(data){
+    //     console.log(data)
+    // })
     .catch(error => {
-        console.error('Ошибка при отправке запроса:', error);
+        console.error('Error sending a request:', error);
     });
-}
+})
